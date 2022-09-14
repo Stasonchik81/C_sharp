d5769eadddfdfb03ded734509cca7f674f7a4817
stanislav <stanislavmuratov@yandex.ru>
2022-09-14 22:54:38 +0300

    Add sum

--- Project001/Program.cs
+++ Project001/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сумма двух чисел
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = num1 + num2;
+Console.Write("Сумма чисел равна: ");
+Console.WriteLine(sum);
