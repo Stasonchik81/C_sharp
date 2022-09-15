bee1906edc768261d9b7a47f8857395affb096fc
stanislav <stanislavmuratov@yandex.ru>
2022-09-15 23:01:00 +0300

    Add Project 002 - div

--- Project002/Program.cs
+++ Project002/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите делимое: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите делитель: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = num1 / num2;
+Console.Write("Результат деления: ");
+Console.WriteLine(res);
+Console.ReadKey();
