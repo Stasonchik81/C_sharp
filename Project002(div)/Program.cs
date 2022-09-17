ab25d1f3ebc07a6b48646a038e0179b24a06285a
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 14:11:00 +0300

    Add random

--- Project002(div)/Program.cs
+++ Project002(div)/Program.cs
@@ -1,8 +1,10 @@
 ﻿// Divide
-Console.Write("Введите делимое: ");
-double num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите делитель: ");
-double num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите делимое: ");
+// double num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите делитель: ");
+// double num2 = Convert.ToInt32(Console.ReadLine());
+int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(1, 10);
 double res = num1 / num2;
 Console.Write("Результат деления: ");
 Console.WriteLine(res);
