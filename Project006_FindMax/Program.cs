f89f5d15a2ca06fbddd72604608cf8f89b2d84e9
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 16:28:03 +0300

    Find max

---
+++ Project006_FindMax/Program.cs
@@ -0,0 +1,10 @@
+﻿// Find max
+int[] arr = {3, 5, 8, 4, 2, 7};
+int max = arr[0];
+for (int i = 0; i < arr.Length; i++)
+{
+    max = arr[i]>max ? arr[i] : max;
+}
+Console.Write("Максимальное значение массива: ");
+Console.Write(max);
+
