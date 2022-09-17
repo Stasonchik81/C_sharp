6bd1866179d4f5d3678cc93a3fcf0ac3ac85efaf
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 15:47:46 +0300

    Hello Masha

---
+++ Project005(Hello Masha)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Hello Masha
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
