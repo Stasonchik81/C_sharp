e18e4b570ac11865d834891888af8b1fe19b3672
stanislav <stanislavmuratov@yandex.ru>
2022-09-17 19:09:39 +0300

    Add Magic - Fractal

---
+++ Project007_Magic/Program.cs
@@ -0,0 +1,42 @@
+﻿// Magic
+Console.Clear();
+
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = yb;
+int count = 0;
+
+while (count<2000)
+{
+    int s = new Random().Next(0, 3);
+    switch (s)
+    {
+        case 0:
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+        break;
+        case 1:
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+        break;
+        case 2:
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+        break;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
